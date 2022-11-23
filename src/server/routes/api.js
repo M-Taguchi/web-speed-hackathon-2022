fb77b21cae0213532fd3e0f67719499d8cefe8ad
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2022-11-23 14:36:02 +0900

    webp形式にapiレスポンスを変換

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -40,7 +40,7 @@ export const apiRoute = async (fastify) => {
   });
 
   fastify.get("/hero", async (_req, res) => {
-    const url = assets("/images/hero.jpg");
+    const url = assets("/images/hero.webp");
     const hash = Math.random().toFixed(10).substring(2);
 
     res.send({ hash, url });
@@ -79,8 +79,15 @@ export const apiRoute = async (fastify) => {
       });
     }
 
-    const races = await repo.find({
+    let races = await repo.find({
       where,
+    });
+
+    races = races.map((race) => {
+      return {
+        ...race,
+        image: race.image.replace(".jpg", ".webp"),
+      };
     });
 
     res.send({ races });
@@ -96,6 +103,8 @@ export const apiRoute = async (fastify) => {
     if (race === undefined) {
       throw fastify.httpErrors.notFound();
     }
+
+    race.image = race.image.replace(".jpg", ".webp");
 
     res.send(race);
   });
